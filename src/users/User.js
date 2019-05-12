6e4c81a3ca036154570752732bd90192f361f512
ColbyWTaylor <colbyt.093@gmail.com>
2019-05-12 02:36:39 +0000

    functional props example

--- src/users/User.js
+++ src/users/User.js
@@ -2,16 +2,15 @@ import React from "react";
 
 const User = props => {
 	let age = props.age ? props.age : "Unavailable";
-    if(props.children) {
-	return (
-		<div>
-			Name: {props.children} | Age {age}
-		</div>
-    } else {
-        return() {
-            <div>Invalid Entry</div>
-        }
-	);
+	if (props.children) {
+		return (
+			<div>
+				Name: {props.children} | Age {age}
+			</div>
+		);
+	} else {
+		return <div>Invalid Entry</div>;
+	}
 };
 
 export default User;
